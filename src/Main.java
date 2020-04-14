783f39dda652f64bc82ee07849b397c904746f04
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-04-14 13:39:33 -0700

    oop1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import oop1.Car;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car benz = new Car();
+
+    }
+}
---
+++ src/oop1/Car.java
@@ -0,0 +1,11 @@
+package oop1;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+}
