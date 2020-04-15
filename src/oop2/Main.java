308331c7e181eac95ced38cb6744b2651051b8e9
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-04-15 16:51:33 -0700

    encapsulation usage

--- src/oop2/Main.java
+++ src/oop2/Main.java
@@ -4,18 +4,18 @@ package oop2;
 public class Main {
 
     public static void main(String[] args) {
+        //composition eg 1
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case theCase = new Case("2208", "Dell", "240", dimensions);
 
-//        Dimensions dimensions = new Dimensions(20, 20, 5);
-//        Case theCase = new Case("2208", "Dell", "240", dimensions);
-//
-//        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
-//
-//        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
-//
-//        PC thePC = new PC(theCase, theMonitor, theMotherboard);
-//        thePC.powerUp();
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
 
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
 
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+        thePC.powerUp();
+
+        //composition eg 2
         Wall wall1 = new Wall("West");
         Wall wall2 = new Wall("East");
         Wall wall3 = new Wall("South");
@@ -33,6 +33,10 @@ public class Main {
         bedRoom.makeBed();
 
         bedRoom.getLamp().turnOn();
+
+
+
+
 
     }
 }
