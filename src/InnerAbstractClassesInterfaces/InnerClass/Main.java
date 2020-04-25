d33aa6aae4a63771bdc035f766d96f2f15fb360b
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-04-25 13:37:07 -0700

    innerclasses

---
+++ src/InnerAbstractClassesInterfaces/InnerClass/Main.java
@@ -0,0 +1,25 @@
+package InnerAbstractClassesInterfaces.InnerClass;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
+
+    public static void main(String[] args) {
+
+        class ClickListener implements Button.OnClickListener {
+            public ClickListener() {
+                System.out.println("I've been attached");
+            }
+
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        }
+
+        btnPrint.setOnClickListener(new ClickListener());
+    }
+
+}
