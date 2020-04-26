09af25aacce84a931cc342dbaf2d26dfa79434f3
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-04-26 11:59:11 -0700

    abstract class challenge

--- src/InnerAbstractClassesInterfaces/AbstractCLass/Main.java
+++ src/InnerAbstractClassesInterfaces/AbstractCLass/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Dog dog = new Dog("Yorkie");
-  
+
         dog.eat();
 
         Parrot parrot = new Parrot("Australian ringneck");
