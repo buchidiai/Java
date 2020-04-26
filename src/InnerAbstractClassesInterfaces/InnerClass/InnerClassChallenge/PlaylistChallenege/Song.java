2e804a6aac32e67db18388e0f1a24baa4c52d60b
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-04-25 17:04:34 -0700

    implemeted inner class for challenge

---
+++ src/InnerAbstractClassesInterfaces/InnerClass/InnerClassChallenge/PlaylistChallenege/Song.java
@@ -0,0 +1,21 @@
+package InnerAbstractClassesInterfaces.InnerClass.InnerClassChallenge.PlaylistChallenege;
+
+public class Song {
+    private String title;
+    private double Duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.Duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        String info = this.title + " Duration: "+ this.Duration;
+        return info;
+    }
+}
