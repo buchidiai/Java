b886b39f9441fd55476346f07b1e759e85a51bfd
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-04-28 15:35:32 -0700

    package challenge

---
+++ src/NamingPackagesStaticFinal/com/example/game/ISaveable.java
@@ -0,0 +1,10 @@
+package NamingPackagesStaticFinal.com.example.game;
+
+import java.util.List;
+
+public interface ISaveable {
+    List<String> write();
+
+    void read(List<String> savedValues);
+
+}
